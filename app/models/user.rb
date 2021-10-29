27aae1a5f420e1cf9900ee91ac32bbacabe6875c
mwilson <mjwilsonmd@gmail.com>
2021-10-29 15:25:10 -0400

    user model complete

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true 
+    validates :password_digest, :session_token, presence: true 
+    validates :password, length: {minimum: 6}, allow_nil: true
+    validates :name, default: :username 
+    # dont know if this is right ^
+    after_initialize :ensure_session_token 
+    attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+    
+end
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(
+        <h1> welcome </h1>, 
+        root);
+})
