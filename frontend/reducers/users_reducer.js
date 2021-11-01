84a2b23911bda9d8efe58d3c05fbc11b81380da3
mwilson <mjwilsonmd@gmail.com>
2021-11-01 10:41:31 -0400

    remove user from state after logout

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENT_USER } from '../actions/session_actions'
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions'
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
@@ -7,6 +7,8 @@ const usersReducer = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, nextState, { [action.currentUser.id]: action.currentUser });
+        case LOGOUT_CURRENT_USER:
+            return {};
         default:
             return state;
     }
