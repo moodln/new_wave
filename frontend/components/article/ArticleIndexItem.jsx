23e273eeead2582348f723109db50973234b87a7
mwilson <mjwilsonmd@gmail.com>
2021-11-05 08:46:54 -0400

    article captions fixed to images

--- frontend/components/article/ArticleIndexItem.jsx
+++ frontend/components/article/ArticleIndexItem.jsx
@@ -3,17 +3,14 @@ import React from "react";
 
 const ArticleIndexItem = (props) => {
     const {article, idx} = props;
-    let name;
-    if (idx === 0) {
-        name = 'first-article' 
-    } else { 
-        name = 'side-article'
-    }
+
     return (
         <div className={`article class${idx}`}>
-            <h2>{article.title}:</h2>
-            <p>{article.description}</p>
             <img className={`img${idx}`} src={article.photoUrl} alt={article.title} />
+            <div className={`container-${idx}`}>
+                <h2>{article.title}:</h2>
+                <p>{article.description}</p>
+            </div>
             <button>explore</button>
         </div>
     )
