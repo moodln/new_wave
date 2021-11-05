4049792939ac87fc7158aad41d031de249c16037
mwilson <mjwilsonmd@gmail.com>
2021-11-05 10:49:52 -0400

    add songs to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_002339) do
+ActiveRecord::Schema.define(version: 2021_11_05_144819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 2021_11_04_002339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_articles_on_title"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+    t.index ["title"], name: "index_songs_on_title"
   end
 
   create_table "users", force: :cascade do |t|
