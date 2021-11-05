dcb9b29cf34ee5f38422a2a715788c4e388d1d6c
mwilson <mjwilsonmd@gmail.com>
2021-11-05 08:11:33 -0400

    article layout complete

--- frontend/components/article/ArticleIndexItem.jsx
+++ frontend/components/article/ArticleIndexItem.jsx
@@ -3,12 +3,18 @@ import React from "react";
 
 const ArticleIndexItem = (props) => {
     const {article, idx} = props;
+    let name;
+    if (idx === 0) {
+        name = 'first-article' 
+    } else { 
+        name = 'side-article'
+    }
     return (
-        <div className='article'>
-                <h2>{article.title}:</h2>
-                <p>{article.description}</p>
-                <img className={`name${idx}`} src={article.photoUrl} alt={article.title} />
-                <button>explore</button>
+        <div className={`article class${idx}`}>
+            <h2>{article.title}:</h2>
+            <p>{article.description}</p>
+            <img className={`img${idx}`} src={article.photoUrl} alt={article.title} />
+            <button>explore</button>
         </div>
     )
 }
