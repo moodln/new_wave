bd0140ece4abcc0ebe46a40d7961ac63d42e46b7
mwilson <mjwilsonmd@gmail.com>
2021-11-11 18:33:25 -0500

    edit stying to include footer

--- frontend/components/footer/Footer.jsx
+++ frontend/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className='footer'>
-            <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a>from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             
             <a href='https://github.com/moodln' ><img src={window.github} alt="" /></a>
         </div>
