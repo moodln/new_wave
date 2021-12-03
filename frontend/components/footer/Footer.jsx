a57719e4835cf5bd1a712fa1ef580b5886b4deff
mwilson <mjwilsonmd@gmail.com>
2021-12-03 15:00:59 -0500

    restyling css for images to be more responsive

--- frontend/components/footer/Footer.jsx
+++ frontend/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='footer'>
             <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a>from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             
-            <a href='https://github.com/moodln' ><img src={window.github} alt="" /></a>
+            <a href='https://github.com/moodln/new_wave' ><img src={window.github} alt="" /></a>
         </div>
     )
 }
