3542736ad0982f7f529ae5ae5ddaff116e5b921e
mwilson <mjwilsonmd@gmail.com>
2021-12-07 15:03:44 -0500

    error message styling complete

--- frontend/components/session/SessionForm.jsx
+++ frontend/components/session/SessionForm.jsx
@@ -48,16 +48,19 @@ class SessionForm extends React.Component {
         let content;
         let button;
         let ex;
+        let email;
         
         if (formType === 'login') {
             content = 'Log in';
             button = 'Log in';
             ex = '';
+            email = 'Email';
             
         } else {
             content = 'Sign up for a new wave account';
             button = 'Sign up';
             ex = <span>&#10005;</span>;
+            email = 'Email address';
         }
         
         return ( 
@@ -69,7 +72,7 @@ class SessionForm extends React.Component {
                 <form onSubmit={this.handleSubmit} className={`session-form ${formType}`}>
                     <div className='session-header'>
                         <h1>{content}</h1>
-                        <p>{ex}</p>
+                        <div onClick={() => this.handleClose()}><p>{ex}</p></div>
                     </div>
                     <div className='session-input'>
                         <div className="sesion-info">
@@ -78,7 +81,7 @@ class SessionForm extends React.Component {
                                     value={this.state.username} 
                                     onChange={this.update('username')} />
                             </label>
-                            <label>Email address
+                            <label>{email}
                                 <input type="text"
                                     value={this.state.email}
                                     onChange={this.update('email')} />  
